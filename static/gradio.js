7e48301e2b9597748aa89897018bff96a56076b3
mv-lab <infomar97@gmail.com>
2024-01-30 02:02:22 +0100

    add website

---
+++ static/gradio.js
@@ -0,0 +1,10 @@
+
+
+function make_script(src) {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.setAttribute("crossorigin", "");
+    script.src = src;
+    document.head.appendChild(script);
+}
+make_script("https://gradio.s3-us-west-2.amazonaws.com/3.27.0/assets/index-9405f928.js");
